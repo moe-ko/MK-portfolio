551edacbb46057dd8584ec1ae5cc630a1d90d8e6
Moe <moemyintko@outlook.com>
2023-04-29 07:44:52 +0100

    font chanages

--- components/home/experience/Experience.js
+++ components/home/experience/Experience.js
@@ -31,31 +31,5 @@ const experience = [
       "MYSQL",
     ],
   },
-  {
-    title: "Facebook",
-    position: "Software Engineer",
-    time: "2020 - 2022",
-    location: "San Francisco",
-    description:
-      "Senior Full Stack Engineer for Facebook Marketplace. Created a service from scratch to handle new listings. Also played some ping pong. It was fun.",
-    tech: ["React", "Node", "PHP", "GraphQL", "Presto", "Cassandra"],
-  },
-  {
-    title: "Amazon",
-    position: "Software Engineer",
-    time: "2016 - 2020",
-    location: "Seattle",
-    description:
-      "Data Engineer for store front. Built data pipelines with Spark and Airflow. Saw Bezos one time. He said he liked my paintings. I said I liked his hair.",
-    tech: [
-      "Java",
-      "Python",
-      "S3",
-      "RDS",
-      "Redshift",
-      "PySpark",
-      "HDFS",
-      "Apache Airflow",
-    ],
-  },
+  
 ];
